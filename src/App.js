afa4fb198141da013fc2c2f64e67de67f5f6cd2f
fardowsa123 <--fardowsa.abdullahi@student.moringaschool.com>
2024-05-05 09:07:42 +0300

    added searchbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import Searchbar from './components/Searchbar';
+import Searchbar from './components/Searchbar/Searchbar';
+import './components/Searchbar/style.css';
 function App() {
   return (
     <>
-
-    <h1>Group 2 Elite Team</h1>
+<div className='App'>
+  <div className='Searchbar'></div>
+  
+</div>
+    
   <Searchbar />
     </>
   );
