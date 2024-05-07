22d980a8ce58b61bca2b47160aaf744d57524ec0
fardowsa123 <--fardowsa.abdullahi@student.moringaschool.com>
2024-05-07 17:37:15 +0300

    searchbar

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -4,40 +4,20 @@ import './style.css';
 
 const Searchbar = () => {
   const [input, setInput] = useState("");
-  //const fetchData = (value) => {
-    //fetch("")
-//    .then((response) => response.json())
-    //.then((json) => {
-
-     // const results = json.filter((user) => {
-        //return (
-        //value &&
-        // user && 
-        // user.name && user.name.tolowerCase().includes(value)
-      //  );
-     // })
-     // console.log(results);
-    //});
-  //}             
-
-//const handleChange = (value) => {
- // setInput(value);
- // fetchData(value);
-
-
-//};
   return (
-    <div className='input-wrapper'>
-      <FaSearch className='search-icon' />
+   <div className='input-wrapper'>
+     <FaSearch className='search-icon' />
       <input
         type='text'
         placeholder='Search movie here...'
-        value={input}
+      value={input}
         onChange={(e) => setInput(e.target.value)}
         className='search-input'
-      />
+     />
     </div>
   )
 }
 
 export default Searchbar;
+
+
