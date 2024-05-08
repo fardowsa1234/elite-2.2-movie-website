3ae9353a9a39d6be478ea8c9d5f690598c286a4b
Jan Muhachi <jan.muhachi@gmail.com>
2024-05-08 14:09:28 +0300

    edited

---
+++ App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import MovieDetails from './components /MovieDetails';
+;
+
+function App() {
+  return (
+    <>
+<div className='App'>
+  <div className='MovieDeatails'></div>
+ 
+</div>
+    
+  <MovieDetails />
+ 
+    </>
+  );
+}
+
+export default App;
